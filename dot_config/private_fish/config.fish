92b4dc0dce5b5932d897105342581940e9be75d2
Kaspar Martin Suursalu <kasparsu@gmail.com>
2026-03-02 13:47:21 +0200

    Update fish config with aliases and remove greeting

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,4 +1,11 @@
 if status is-interactive
+set -g fish_greeting
+alias ls='eza --color=auto --icons=always'
+alias zed='zeditor'
+alias vim='nvim'
+alias cat='bat --color=auto'
+alias c='chezmoi'
 # Commands to run in interactive sessions can go here
 end
 starship init fish | source
+zoxide init --cmd=cd fish | source
